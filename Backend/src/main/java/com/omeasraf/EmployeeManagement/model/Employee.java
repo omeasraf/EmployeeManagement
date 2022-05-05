5f4e00d1465b13eeb7f1e089e0c36a30649330cc
Ome Asraf <omeasraf0@gmail.com>
2022-05-05 11:36:04 -0400

    Updated Java version

---
+++ Backend/src/main/java/com/omeasraf/EmployeeManagement/model/Employee.java
@@ -0,0 +1,121 @@
+package com.omeasraf.EmployeeManagement.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String jobTitle;
+    private Date joinDate;
+    private String phoneNumber;
+    private String imageURL;
+    @Column(nullable = false, updatable = false)
+    private String employeeID;
+
+    public Employee(Long id, String firstName, String lastName, String email, String jobTitle, Date joinDate, String phoneNumber, String imageURL, String employeeID) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.joinDate = joinDate;
+        this.phoneNumber = phoneNumber;
+        this.imageURL = imageURL;
+        this.employeeID = employeeID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", joinDate=" + joinDate +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+                ", employeeID='" + employeeID + '\'' +
+                '}';
+    }
+}
